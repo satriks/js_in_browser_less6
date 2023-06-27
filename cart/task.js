117432f4c72473d715e71140c8a9ec28e5e19292
Yury Vereskun <satriks@mail.ru>
2023-06-27 13:14:06 +0300

    done cart storage

--- cart/task.js
+++ cart/task.js
@@ -1,11 +1,12 @@
-document.querySelectorAll('.product__quantity-control').forEach(el => el.addEventListener('click', changeQuantity))
-document.querySelectorAll('.product__add').forEach(el => el.addEventListener('click', addToCart))
-let myIntrval = 0;
-
-
-
-function changeQuantity() {
-
+(function () {
+    
+    document.addEventListener('DOMContentLoaded', printFromStorage)
+    document.querySelectorAll('.product__quantity-control').forEach(el => el.addEventListener('click', changeQuantity))
+    document.querySelectorAll('.product__add').forEach(el => el.addEventListener('click', addToCart))
+    let myIntrval = 0;
+    
+    function changeQuantity() {
+        
     let symbol = Array.from(this.classList)
     let quant = Number(this.closest('.product__quantity-controls').querySelector('.product__quantity-value').innerText)
     
@@ -14,7 +15,7 @@ function changeQuantity() {
     } else if (symbol.includes('product__quantity-control_dec')) {
         quant > 1? quant-- : quant  
     }
-
+    
     this.closest('.product__quantity-controls').querySelector('.product__quantity-value').innerText = quant   
 }
 
@@ -24,7 +25,7 @@ function addToCart() {
     const picture = this.closest('.product').querySelector('.product__image');
     const cart = document.querySelector('.cart__products')
     const prodIdInCart = Array.from(document.querySelectorAll('.cart__product')).map(el => el.dataset.id)
-
+    
     let div = document.createElement('div')
     div.innerHTML = `
     <div class="cart__product" data-id="${id}">
@@ -34,45 +35,42 @@ function addToCart() {
         <div class="cart-control">
             <div class="minus">-</div>
             <div class="plus">+</div>
-        </div>   
-        </div>`
-        console.log(this);
-        
-    // console.log(prodIdInCart);
-    // console.log(id.toString());
-
-    prodIdInCart.includes(id)? document.querySelector(`[data-id="${id}"]`).querySelector('.cart__product-count').innerText = Number(document.querySelector(`[data-id="${id}"]`).querySelector('.cart__product-count').innerText) + Number(quantity):  cart.append(div);   
+            </div>   
+            </div>`
+            
+            
+            prodIdInCart.includes(id)? document.querySelector(`[data-id="${id}"]`).querySelector('.cart__product-count').innerText = Number(document.querySelector(`[data-id="${id}"]`).querySelector('.cart__product-count').innerText) + Number(quantity):  cart.append(div);   
     prodIdInCart.includes(id)? div = document.querySelector(`[data-id="${id}"]`) : div = div
-    if (cart.children.length > 0){
-        console.log(cart.children, 'chil');
-        Array.from( cart.children).forEach(el => el.querySelector(".plus").addEventListener('click', cartQuantity))
-        Array.from( cart.children).forEach(el => el.querySelector(".minus").addEventListener('click', cartQuantity))
-    }
-    cart.querySelectorAll('.cart__product-close').forEach(el => el.addEventListener('click', removeProduct))
-    document.querySelector('.cart').classList.remove('hidden')
+    
+    cartCheck(cart)
+    
     createPicture(picture, div)
+    
+    saveToStorage()
 }  
 
 function cartQuantity() {
     const quantity = Number(this.parentElement.previousElementSibling.innerText)
-
+    
     if (this.classList.value == 'plus') {
         this.parentElement.previousElementSibling.innerText =quantity + 1
     } else if (this.classList.value == 'minus'){
         if (quantity > 1) {
-        this.parentElement.previousElementSibling.innerText = quantity - 1
+            this.parentElement.previousElementSibling.innerText = quantity - 1
         } else {
             this.closest('.cart__product').parentElement.remove()
             checkCart()
 
         }
     }
+    saveToStorage()
 }
 
 function removeProduct() {
     this.closest('.cart__product').parentElement.remove()
     checkCart()
-
+    saveToStorage()
+    
 }
 
 function checkCart() {
@@ -84,7 +82,7 @@ function checkCart() {
 }
 
 function createPicture(picture, goal) {
-
+    
     const img = document.createElement('img')
     img.src = picture.src
     img.classList.add('move-pict')
@@ -94,35 +92,73 @@ function createPicture(picture, goal) {
     img.style.width = "100px"
     img.style.objectFit = "contain"
     document.querySelector('.product').append(img)
-
-    console.log(picture,  goal , 'tagr');
-    console.log(goal.offsetLeft, picture.offsetLeft,  'offsets');
     
     const topOffset = (goal.offsetTop - picture.offsetTop) / 30
     const leftOffset = (goal.offsetLeft - picture.offsetLeft) / 30
-
+    
     myIntrval = setInterval ((() => movePicture(leftOffset, topOffset, img, goal.offsetTop)), 2)
 }
 
 function movePicture(left, top, img, stop) {
-
+    
     if (stop > img.offsetTop) {
         clearInterval(myIntrval)
         img.remove()
     }
-
-    console.log(left, top, img, stop, 'paramssssssss');
-
+    
     img.style.top = `${img.offsetTop + top}px`
     img.style.left = `${img.offsetLeft + left}px`
     
 }
 
 function saveToStorage() {
-    //Хранение
+    let cratList = []
+    const cart = document.querySelector('.cart__products')
     
+    for (let i of Array.from(cart.children)) {
+        let x = {
+            id : i.firstElementChild.dataset.id,
+            count: i.querySelector('.cart__product-count').innerText,
+            picture: i.querySelector('.cart__product-image').src,
+            
+        }
+        cratList.push(x)
+    }
+    
+    localStorage.setItem('product', JSON.stringify(cratList))
 }
 
 function printFromStorage() {
-    //Отрисовка
+    const products = JSON.parse(localStorage.getItem('product')) || []
+    const cart = document.querySelector('.cart__products')
+    
+    products.forEach(el => {
+        let div = document.createElement('div')
+        div.innerHTML = `
+            <div class="cart__product" data-id="${el.id}">
+            <img class="cart__product-image" src="${el.picture}">
+            <div class="cart__product-close">&times;</div>
+            <div class="cart__product-count">${el.count}</div>
+            <div class="cart-control">
+            <div class="minus">-</div>
+            <div class="plus">+</div>
+            </div>   
+            </div>`
+
+        cart.append(div)
+
+    })
+    cartCheck(cart)
+    
 }
+
+function cartCheck(cart) {
+    if (cart.children.length > 0){
+        Array.from( cart.children).forEach(el => el.querySelector(".plus").addEventListener('click', cartQuantity))
+        Array.from( cart.children).forEach(el => el.querySelector(".minus").addEventListener('click', cartQuantity))
+        cart.querySelectorAll('.cart__product-close').forEach(el => el.addEventListener('click', removeProduct))
+        document.querySelector('.cart').classList.remove('hidden')
+    }
+}
+
+}) ()
